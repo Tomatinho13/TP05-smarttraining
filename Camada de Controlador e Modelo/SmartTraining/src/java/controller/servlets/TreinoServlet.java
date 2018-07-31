ca12e203ffe04ada72c0aa0b35754acbe162c2e3
João Victor Melo <melogalo10@gmail.com>
2018-07-30 23:54:09 -0300

    Adicionando todos os servlets implementados e cheios de erros. Aparentemente, porém (inserir piscadinha com a língua de fora, tipo isso ;p)

--- Camada de Controlador e Modelo/SmartTraining/src/java/controller/servlets/TreinoServlet.java
+++ Camada de Controlador e Modelo/SmartTraining/src/java/controller/servlets/TreinoServlet.java
@@ -1,86 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.dao.TreinoDao;
+import model.classes.Treino;
 
 /**
  *
- * @author melog
+ * @author Tomatinho
  */
+
 public class TreinoServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TreinoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TreinoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+   @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TreinoDao treinoDao = new TreinoDao();
+        Gson gson = new Gson();
+        String cpf = request.getParameter("cpf");
+        try {
+            if(treinoDao.getTreino(cpf)==null){
+                response.sendError(404);
+            }
+            else{
+                response.setContentType("application/json");
+                response.getWriter().write(gson.toJson(treinoDao.getTreino(cpf)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinoServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TreinoDao treinoDao = new TreinoDao();
+        Gson gson = new Gson();
+        try {
+            treinoDao.postTreino(gson.fromJson(request.getReader(), Treino.class));
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.setStatus(200);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TreinoDao treinoDao = new TreinoDao();
+        Gson gson = new Gson();
+        try {
+            treinoDao.putTreino(gson.fromJson(request.getReader(), Treino.class));
+        } catch (SQLException ex) {
+            Logger.getLogger(TreinoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.setStatus(200);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TreinoDao treinoDao = new TreinoDao();
+        Gson gson = new Gson();
+        String nroFicha = request.getParameter("nroFicha");
+        String nroTreino = request.getParameter("nroTreino");
 
+        treinoDao.deleteTreino(gson.fromJson(request.getReader(), Treino.class), nroFicha, nroTreino);
+        response.setStatus(200);
+    }
 }
