4ed800c9477d940cf40e8a0e172a8e2a2849fcdb
João Victor <melogalo10@gmail.com>
2018-08-02 10:55:34 -0300

    Alterando classes do core

--- Camada de Modelo/smarttraining-core/src/br/cefetmg/inf/model/services/impl/ManterAvaliacao.java
+++ Camada de Modelo/smarttraining-core/src/br/cefetmg/inf/model/services/impl/ManterAvaliacao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.services.impl;
+package br.cefetmg.inf.model.services.impl;
 
 /**
  *
