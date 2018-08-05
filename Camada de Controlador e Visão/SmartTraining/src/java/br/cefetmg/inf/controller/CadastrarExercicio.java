6a38fcaed336ad8b910ce59297c44568ec50a419
Gabriel <gabriel44558888@gmail.com>
2018-08-05 20:53:32 -0300

    Iniciei o esboço do servlet e corrigi um controller

--- Camada de Controlador e Visão/SmartTraining/src/java/br/cefetmg/inf/controller/CadastrarExercicio.java
+++ Camada de Controlador e Visão/SmartTraining/src/java/br/cefetmg/inf/controller/CadastrarExercicio.java
@@ -7,17 +7,18 @@ import br.cefetmg.inf.model.services.IManterExercicio;
 import br.cefetmg.inf.model.services.impl.ManterAparelho;
 import br.cefetmg.inf.model.services.impl.ManterExercicio;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 public class CadastrarExercicio {
-    public static String execute(HttpServletRequest request) throws SQLException {
+    public static String execute(HttpServletRequest request){
         String jsp = "";
         
+        try{
         String nomeExercicio = request.getParameter("nomeExercicio");
         String descricaoExercicio = request.getParameter("descExercicio");
+        int codMusculo = Integer.parseInt(request.getParameter("codMusculo"));
         
         String nomeAparelho = request.getParameter("nomeAparelho");
         IManterAparelho manterAparelho = new ManterAparelho();
@@ -27,12 +28,11 @@ public class CadastrarExercicio {
         IManterExercicio manterExercicio = new ManterExercicio();
         Exercicio exercicio = new Exercicio();
 
-        exercicio.setCodExercicio();
         exercicio.setDescricaoExercicio(descricaoExercicio);
         exercicio.setNomeExercicio(nomeExercicio);
         
-        try {
-            manterExercicio.cadastrar(exercicio);
+        manterExercicio.cadastrar(exercicio, codMusculo);
+        
         } catch (SQLException ex) {
             Logger.getLogger(CadastrarAluno.class.getName()).log(Level.SEVERE, null, ex);
         }
--- Camada de Controlador e Visão/SmartTraining/src/java/br/cefetmg/inf/controller/ServletWeb.java
+++ Camada de Controlador e Visão/SmartTraining/src/java/br/cefetmg/inf/controller/ServletWeb.java
@@ -1,88 +1,44 @@
 package br.cefetmg.inf.controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Aluno
- */
 public class ServletWeb extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletWeb</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletWeb at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private String jsp = "";
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+        String acao = request.getParameter("acao");
+        
+        if(acao.equals("Logar"))
+            jsp = FazerLogin.execute(request);
+        else if (acao.equals("CadastrarAluno"))
+            jsp = CadastrarAluno.execute(request);
+        else if (acao.equals("CadastrarInstrutor"))
+            jsp = CadastrarInstrutor.execute(request);
+        else if (acao.equals("CadastrarAvaliacao"))
+            jsp = CadastrarAvaliacao.execute(request);
+        else if (acao.equals("CadastrarExercicio"))
+            jsp = CadastrarExercicio.execute(request);
+        else if (acao.equals("ListarAvaliacoes"))
+            jsp = ListarAvaliacoes.execute(request);
+        else if (acao.equals("ListarExercicios"))
+            jsp = ListarExercicios.execute(request);
+        else if (acao.equals("ListarUsuarios"))
+            jsp = ListarUsuarios.execute(request);
+        else if (acao.equals("RemoverExercicio"))
+            jsp = RemoverExercicio.execute(request);
+        else if (acao.equals("RemoverUsuario"))
+            jsp = RemoverUsuario.execute(request);
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        //Redirecionando pagina
+        RequestDispatcher rd = request.getRequestDispatcher(jsp);
+        rd.forward(request, response);
+    }    
 }
