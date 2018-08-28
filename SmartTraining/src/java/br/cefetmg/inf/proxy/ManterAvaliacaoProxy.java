4529cca1867c54887b477d5b4333dc0ffc40de88
Felipe Gonçalves <goncalves.pipo2123@gmail.com>
2018-08-28 20:24:00 -0300

    Adicionando o proxy de Avaliação, o último foi de Atividade (xD)

--- SmartTraining/src/java/br/cefetmg/inf/proxy/ManterAvaliacaoProxy.java
+++ SmartTraining/src/java/br/cefetmg/inf/proxy/ManterAvaliacaoProxy.java
@@ -5,10 +5,85 @@
  */
 package br.cefetmg.inf.proxy;
 
+import br.cefetmg.inf.model.domain.Avaliacao;
+import br.cefetmg.inf.model.services.IManterAvaliacao;
+import br.cefetmg.inf.util.Pacote;
+import br.cefetmg.inf.util.TipoOperacao;
+import com.google.gson.Gson;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
- * @author Celio
+ * @author felipe
  */
-public class ManterAvaliacaoProxy {
+public class ManterAvaliacaoProxy implements IManterAvaliacao{
+
+    @Override
+    public ArrayList<Avaliacao> pesquisarPorAluno(String codCpf) throws SQLException {
+        Pacote pacoteEnviado;
+        Pacote pacoteRecebido;
+        
+        Gson gson = new Gson();
+        
+        pacoteEnviado = new Pacote(TipoOperacao.LISTA_AVALIACAO_ALUNO, gson.toJson(codCpf));
+        
+        pacoteRecebido = cliente.request(pacoteEnviado);
+        ArrayList<Avaliacao> listaAvaliacoes = gson.fromJson(pacoteRecebido.getDados(), ArrayList.class);
+        return listaAvaliacoes;
+    }
+
+    @Override
+    public Avaliacao pesquisar(String codCpf, LocalDate data) throws SQLException {
+        Pacote pacoteEnviado;
+        Pacote pacoteRecebido;
+        
+        Gson gson = new Gson();
+        
+        //MAIS DE UM PARAMETRO
+        pacoteEnviado = new Pacote(TipoOperacao.PESQ_AVALIACAO, gson.toJson(codCpf));
+        
+        pacoteRecebido = cliente.request(pacoteEnviado);
+        Avaliacao avaliacao = gson.fromJson(pacoteRecebido.getDados(), Avaliacao.class);
+        return avaliacao;
+    }
+
+    @Override
+    public void cadastrar(Avaliacao avaliacao) throws SQLException {
+        Pacote pacoteEnviado;
+        Pacote pacoteRecebido;
+        
+        Gson gson = new Gson();
+        
+        pacoteEnviado = new Pacote(TipoOperacao.CAD_AVALIACAO, gson.toJson(avaliacao));
+        
+        cliente.request(pacoteEnviado);
+    }
+
+    @Override
+    public void alterar(Avaliacao avaliacao) throws SQLException {
+        Pacote pacoteEnviado;
+        Pacote pacoteRecebido;
+        
+        Gson gson = new Gson();
+        
+        pacoteEnviado = new Pacote(TipoOperacao.ALTERA_AVALIACAO, gson.toJson(avaliacao));
+        
+        cliente.request(pacoteEnviado);
+    }
+
+    @Override
+    public void excluir(String codCpf, LocalDate datAvaliacao) throws SQLException {
+        Pacote pacoteEnviado;
+        Pacote pacoteRecebido;
+        
+        Gson gson = new Gson();
+        
+        //MAIS DE UM PARAMETRO
+        pacoteEnviado = new Pacote(TipoOperacao.EXCLUI_AVALIACAO, gson.toJson(codCpf));
+        
+        cliente.request(pacoteEnviado);
+    }
     
 }
