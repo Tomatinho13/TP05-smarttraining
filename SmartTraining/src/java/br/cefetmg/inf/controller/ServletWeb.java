65d92ba881d5c0cf770244872ba1ebde9a6c11bb
JamalzGemaplys <gabriel44558888@gmail.com>
2018-11-02 17:04:40 -0300

    adicionei o comando para corrigir erros de CORS

--- SmartTraining/src/java/br/cefetmg/inf/controller/ServletWeb.java
+++ SmartTraining/src/java/br/cefetmg/inf/controller/ServletWeb.java
@@ -55,7 +55,9 @@ public class ServletWeb extends HttpServlet {
         
         Controller controller = determinaController(request);
         jsp = controller.execute(request);
-
+        
+        adicionaCabecalhoCors(response);
+        
         //Redirecionando pagina
         RequestDispatcher rd = request.getRequestDispatcher(jsp);
         rd.forward(request, response);
@@ -82,5 +84,12 @@ public class ServletWeb extends HttpServlet {
         }
 
         return TipoView.HTML;
+    }
+    
+    private void adicionaCabecalhoCors(HttpServletResponse response){
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "1728000");
     }
 }
